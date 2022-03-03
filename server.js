83c1eae1012415c6ca8d7aef8e2cec5710548a15
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-03-03 16:08:40 -0600

    delete call

--- server.js
+++ server.js
@@ -25,20 +25,44 @@ const db = mysql.createConnection(
 // });
 
 // GET a single candidate
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if(err) {
-        console.log(err);
-    }
-    console.log(row);
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = 'SELECT * FROM candidates WHERE id = ?';
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
 });
 
 // delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.delete('/api/candidate/:id', (req,res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if(!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+    
+})
 
 // create a candidate
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
@@ -51,6 +75,22 @@ db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
 //     }
 //     console.log(result);
 // });
+
+// get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 
 // default response for any other request (NOT FOUND)
 app.use((req, res) => {
